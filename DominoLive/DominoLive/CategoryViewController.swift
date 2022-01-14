a9f272c5bc66223c48272f7ebd1378ed65d954e4
songgyeol <songgyeol@kakao.com>
2022-01-14 23:32:59 +0900

    DominoLive_#1

---
+++ DominoLive/DominoLive/CategoryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryViewController.swift
+//  DominoLive
+//
+//  Created by 송결 on 2022/01/14.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ DominoLive/DominoLive/WishViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WishViewController.swift
+//  DominoLive
+//
+//  Created by 송결 on 2022/01/14.
+//
+
+import UIKit
+
+final class WishViewController: UIViewController {
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+}
+
+//MARK: - UI
+extension WishViewController {
+    private func setUI() {
+        setBasic()
+        setLayout()
+    }
+    
+    private func setBasic() {
+        
+    }
+    
+    private func setLayout() {
+        
+    }
+}
